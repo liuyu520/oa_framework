d3c8eaf25577f74551d9768330e49d367af5a528
huangweii <huangweii@chanjet.com>
2016-05-04 21:36:04 +0800

    把 fileName.replaceAll("[\\s]+", SystemHWUtil.EMPTY); 抽取出来为函数

--- src/main/java/oa/util/HWUtils.java
+++ src/main/java/oa/util/HWUtils.java
@@ -5,6 +5,7 @@ import com.common.util.SystemHWUtil;
 import com.common.util.WebServletUtil;
 import com.io.hw.file.util.FileUtils;
 import com.string.widget.util.RandomUtils;
+import com.string.widget.util.RegexUtil;
 import com.string.widget.util.ValueWidget;
 import com.time.util.TimeHWUtil;
 import net.sf.jxls.transformer.XLSTransformer;
@@ -426,11 +427,11 @@ public class HWUtils {
 	}
 
 	public static UploadResult getSavedToFile(HttpServletRequest request, String fileName, String uploadFolder) {
-		fileName = fileName.replaceAll("[\\s]+", SystemHWUtil.EMPTY);//IE中识别不了有空格的json
-		// 保存到哪儿
+        fileName = RegexUtil.filterBlank(fileName);//IE中识别不了有空格的json
+        // 保存到哪儿
 		String finalFileName = TimeHWUtil.formatDateByPattern(TimeHWUtil
-				.getCurrentTimestamp(), "yyyyMMddHHmmss") + "_"
-				+ new Random().nextInt(1000) + "_" + fileName;
+                .getCurrentTimestamp(), TimeHWUtil.yyyyMMddHHmmss) + "_"
+                + new Random().nextInt(1000) + "_" + fileName;
 		String relativePath = null;
 		if (ValueWidget.isNullOrEmpty(uploadFolder)) {
 			relativePath = Constant2.UPLOAD_FOLDER_NAME + "/image";
