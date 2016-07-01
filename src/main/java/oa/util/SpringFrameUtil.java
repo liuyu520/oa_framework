c760d1dc6bcc59c6e7d8c3108a76d1712031694b
huangweii <huangweii@chanjet.com>
2016-07-01 11:04:26 +0800

    把getDao(String dao) 方法移动到SpringMVCUtil 中

--- src/main/java/oa/util/SpringFrameUtil.java
+++ src/main/java/oa/util/SpringFrameUtil.java
@@ -1,24 +1,5 @@
 package oa.util;
 
-import com.common.dao.generic.GenericDao;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import javax.servlet.http.HttpServletRequest;
-
 public class SpringFrameUtil {
-    /***
-     * 根据bean name获取对象
-     *
-     * @param dao
-     * @return
-     */
-    public static GenericDao getDao(String dao) {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(request.getSession().getServletContext());
-        GenericDao accessLogDao = (GenericDao) ctx.getBean(dao);
-        return accessLogDao;
-    }
+
 }
--- src/main/java/oa/util/SpringMVCUtil.java
+++ src/main/java/oa/util/SpringMVCUtil.java
@@ -1,10 +1,13 @@
 package oa.util;
 
+import com.common.dao.generic.GenericDao;
 import com.common.util.WebServletUtil;
 import com.io.hw.json.HWJacksonUtils;
 import org.springframework.ui.Model;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -15,6 +18,18 @@ import java.util.Map;
  * spring MVC 框架专用方法
  */
 public class SpringMVCUtil {
+    /***
+     * 根据bean name获取对象
+     *
+     * @param dao
+     * @return
+     */
+    public static GenericDao getDao(String dao) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(request.getSession().getServletContext());
+        GenericDao accessLogDao = (GenericDao) ctx.getBean(dao);
+        return accessLogDao;
+    }
     public static void saveObject(String sessionKey, Object object) {
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
         if (null == servletRequestAttributes) {
